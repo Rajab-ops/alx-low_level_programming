85798b4a2cc75f42574fa2b7831f346fbc570020
Rajab-ops <ahmedrajab204@gmail.com>
2023-09-07 07:59:14 -0700

    ma

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,11 @@
  * Return: Always return 0 to indicate success
  */
 int main(void)
-{ printf("with proper grammar, but the outcome is a piece of art,\n");
- return(0);
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
+
+
+
+
